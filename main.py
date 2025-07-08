e99e3358f45ad5c056faf9fba9cb26ee51326c7f
Ajith narayana K <ajithnarayana57@gmail.com>
2025-07-08 12:56:07 +0530

    ss

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ class Attempt(BaseModel):
 # ---------------- Routes ----------------
 
 @app.post("/games")
+@app.post("/games/")
 async def create_game(game: GameCreate):
     print("Received Game:", game)
     try:
